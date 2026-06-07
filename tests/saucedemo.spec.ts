1e218f469c81c654fde7fe67807da80799560bd6
Yuliya Volkova <yvolkova@solvd.com>
2026-06-07 20:39:00 +0200

    Fix test.only by removing it

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -126,13 +126,5 @@ test.describe('SauceDemo', () => {
       'Error should appear for locked out user'
     ).toBeVisible();
   });
-  test.only('test with .only - runs only this test', async ({ page }) => {
-    await page.goto('/');
-    await page.getByPlaceholder('Username').fill('standard_user');
-    await page.getByPlaceholder('Password').fill('secret_sauce');
-    await page.getByRole('button', { name: 'Login' }).click();
-
-    await expect(page).toHaveURL(/inventory/);
-  });
-
 });
+ 
